e6fd75bd0e5ff4587ba51b5688dcb19ec9f35f27
bsucs <122378640+bsucs@users.noreply.github.com>
2025-04-11 13:44:48 -0400

    d

--- jobother/macosx/packosx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ant && tar -jxvf macosx/osxpack.tar.bz2 && mv JBother.jar JBother.app/Contents/Resources/Java
-zip -r JBother.zip JBother.app
-
-echo
-echo "Done."
-echo
--- jobother/plugins/OSXPlugin/packosx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ant && tar -jxvf macosx/osxpack.tar.bz2 && mv JBother.jar JBother.app/Contents/Resources/Java
-
-echo
-echo "Done."
-echo
